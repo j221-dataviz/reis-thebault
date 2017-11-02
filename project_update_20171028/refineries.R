278fb0ea9f639169f002bd83b6eddc5eba6dd3c6
paldhous <peter@peteraldhous.com>
2017-11-02 13:35:23 -0700

    some comments/suggestions in R script

--- project_update_20171028/refineries.R
+++ project_update_20171028/refineries.R
@@ -11,11 +11,21 @@ refineries_names <- read_csv('refineries_names.csv')
 # joining the two
 refineries_demo_names <- inner_join(refineries_demo, refineries_names, by=c("frs_id" = "frs_id"))
 
-#deleting all those pesky nulls --- why are they there??
+#deleting all those pesky nulls --- why are they there?? 
+# A - BOTH OF YOUR CSVs have hundreds of empry rows at the end, so every null gets joined to every null from the 
+# other data frame in the join. Let's look at why that was the case, just to be sure nothing went wrong earlyer
 refineries_demo_names <- refineries_demo_names[-c(755:170294),]
+
+# DON'T DO IT BY COUNTING ROWS, INSTEAD:
+refineries_demo_names <- refineries_demo_names %>%
+  filter(!is.na(frs_id))
 
 #reordering columns, so that facility_name is first
 refineries_demo_names <- refineries_demo_names[c(1,46,2:45)]
+
+# YOUR METHOD IS OK, BUT I WOULD DO THIS IN DPLYR ALSO:
+refineries_demo_names <- refineries_demo_names %>%
+  select(1,46,2:45)
 
 #importing toxic release data, must join two 
 tri_release <- read_csv('TRI_petroleum_release.csv')
@@ -25,10 +35,17 @@ tri_ID <- read_csv('tri_frs_ids.CSV')
 refineries_release <- inner_join(tri_ID, tri_release, by=c("V_TRI_FORM_R_EZ.TRI_FACILITY_ID" = "TRIF ID"))
 
 #joining release data with refinery demo data
+# WHY DO WE DROP FROM 754 to 444 REFINERIES HERE. IT'S PROBABLY OK, I JUST WANT TO UNDERSTAND
 refineries_demo_release <- inner_join(refineries_demo_names, refineries_release, by=c("frs_id" = "V_TRI_FORM_R_EZ.FRS_ID"))
 
 #exporting refineries_demo_release for cleaning
 write.csv(refineries_demo_release, file = 'refineries_demo_release.csv', row.names = FALSE)
+# AM I CORRECT IN THINKING THAT THE CLEANING IS ALL CHANGING COLUMN NAMES AND ORDER?
+# IF SO, YOU CAN DO IN R VERY EASILY
+# CHANGE COLUMN NAMES LIKE THIS:
+names(refineries_demo_release) <- c("field1","field2","field3","etc")
+# WHERE FIELD1, FIELD2, FIELD3 ETC ARE YOUR NEW COLUMN NAMES.
+# THEN USE DPLYR SELECT TI CHANGE ORDER LIKE I did in lines 27-28
 
 #bringing clean data back
 refineries_master <- read_csv('refineries_demo_release_clean.csv')
