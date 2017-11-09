521afa777ac7b283c23e9070016bbfd401b297e7
paldhous <peter@peteraldhous.com>
2017-11-09 15:49:02 -0800

    updated census R script and data

--- census.R
+++ census.R
@@ -6,36 +6,39 @@ library(dplyr)
 library(tigris)
 
 # set census api key. Obtain from http://api.census.gov/data/key_signup.html
-census_api_key("your API key here!", install = TRUE)
+census_api_key("ea622cc1e1cfe64aa1d585cd7e2e360f9de089ad", install = TRUE, overwrite=TRUE)
 readRenviron("~/.Renviron")
 
 # get list of variables in the 2015 American Community Survey, 5-year estimates (i.e. 2011-2015)
 acs15 <- load_variables(2015, "acs5", cache = TRUE)
 
+# get list of variables in the 2010 Census (i.e. 2011-2015)
+census10_sf1 <- load_variables(2010, "sf1", cache = TRUE)
+
+
 #############
-# get ACS 2015 5-year data, by counties
+# get 2010 Census data, by state
 
 # population
-population <- get_acs(geography = "state", 
-                      variables = "B01003_001",
-                      year = 2015) %>%
-  rename(population = estimate)  %>%
+population <- get_decennial(geography = "state", 
+                      variables = "P0010001",
+                      year = 2010) %>%
+  rename(population = value)  %>%
   select(1,2,4)
 
 # number of households
-households <- get_acs(geography = "state", 
-                      variables = c("B17017_001"), 
-                      year = 2015) %>%
-  rename(households = estimate) %>%
+households <- get_decennial(geography = "state",
+                            variables = "P0190001",
+                            year = 2010) %>%
+  rename(households = value) %>%
   select(1,2,4)
 
-# white population (white alone, not latino)
-white_population <- get_acs(geography = "state",
-                            variables = c("B01001H_001"),
-                            year = 2015) %>%
-  rename(white_population = estimate) %>%
+# white population (white alone, not hispanic)
+white_population <- get_decennial(geography = "state",
+                            variables = "P0090005",
+                            year = 2010) %>%
+  rename(white_population = value) %>%
   select(1,2,4)
-
 
 # join data frames into one, calculate percentages where necessary
 states <- inner_join(population,households) %>%
